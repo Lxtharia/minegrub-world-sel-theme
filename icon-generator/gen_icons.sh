a4e37ce52d17b84184f2d346ab65a52092ed5391
meap02 <79549780+meap02@users.noreply.github.com>
2024-01-31 13:02:50 -0800

    Add Linux Mint icon

--- icon-generator/gen_icons.sh
+++ icon-generator/gen_icons.sh
@@ -13,7 +13,8 @@ c=windows	&& $cmd $c 'Microsoft Windows (15/07/2015, 6:19 AM)'		'Survival Mode, 
 c=arch		&& $cmd $c 'Arch Linux (11/02/2002, 2:24 AM)'		'Creative Mode, Cheats, Version: 2.5.1' $src$c.png $dst
 c=bedrock	&& $cmd $c 'Bedrock Linux (03/08/2012, 1:34 PM)'		'Adventure Mode, Version: 0.7-Poki' $src$c.png $dst
 c=manjaro	&& $cmd $c 'Manjaro Linux (10/07/2011, 00:38 AM)' 		'Adventure Mode, Cheats, Version 23.0.0' $src$c.png $dst
+c=linuxmint	&& $cmd $c 'Linux Mint (08/27/2006, 6:11 AM)' 		'Adventure Mode, No Cheats, Version 21.3' $src$c.png $dst
+
 # no minecraft logo yet
 src='./distro-icons/distributor-logo-'
 c=debian	&& $cmd $c 'Debian (16/08/1993, 5:33 PM)'		'Survival Mode, Cheats, Version: 12' $src$c.png $dst
-
